963479c4fa8f4974ce9404f606f0c231c6d24ddb
mlleonart <94650399+mlleonart@users.noreply.github.com>
2024-10-15 10:13:39 -0300

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,23 +1,23 @@
 // deck.js
 const initialDeck = [
-    { nombre: "Ferrari 488 GTB", año: 2015, chasis: "Coupé", propulsion: "Gasolina", velocidad: 330, potencia: 670, aceleracion: 3.0, tipo: "Superdeportivo", precio: 280000, 
+    { id: 1, nombre: "Ferrari 488 GTB", año: 2015, chasis: "Coupé", propulsion: "Gasolina", velocidad: 330, potencia: 670, aceleracion: 3.0, tipo: "Superdeportivo", precio: 280000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Ferrari_488_GTB_Toulouse.jpg/800px-Ferrari_488_GTB_Toulouse.jpg?20240511091629" },
-    { nombre: "Tesla Model S Plaid", año: 2021, chasis: "Sedán", propulsion: "Eléctrico", velocidad: 322, potencia: 1020, aceleracion: 1.99, tipo: "Eléctrico", precio: 130000, 
+    { id: 2, nombre: "Tesla Model S Plaid", año: 2021, chasis: "Sedán", propulsion: "Eléctrico", velocidad: 322, potencia: 1020, aceleracion: 1.99, tipo: "Eléctrico", precio: 130000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/d/df/21_Tesla_Model_S_Plaid.jpg" },
-    { nombre: "Porsche 911 GT3 RS", año: 2022, chasis: "Coupé", propulsion: "Gasolina", velocidad: 296, potencia: 525, aceleracion: 3.2, tipo: "Deportivo", precio: 220000, 
+    { id: 3, nombre: "Porsche 911 GT3 RS", año: 2022, chasis: "Coupé", propulsion: "Gasolina", velocidad: 296, potencia: 525, aceleracion: 3.2, tipo: "Deportivo", precio: 220000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Porsche_911_GT3_RS_Weissach.jpg/800px-Porsche_911_GT3_RS_Weissach.jpg?20230413105116"  },
-    { nombre: "Bugatti Chiron", año: 2016, chasis: "Coupé", propulsion: "Gasolina", velocidad: 420, potencia: 1500, aceleracion: 2.4, tipo: "Hiperdeportivo", precio: 3000000, 
+    { id: 4, nombre: "Bugatti Chiron", año: 2016, chasis: "Coupé", propulsion: "Gasolina", velocidad: 420, potencia: 1500, aceleracion: 2.4, tipo: "Hiperdeportivo", precio: 3000000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Bugatti_Chiron_Royal_Blue.jpg/800px-Bugatti_Chiron_Royal_Blue.jpg?20211023215806"  },
-    { nombre: "McLaren 720S", año: 2017, chasis: "Coupé", propulsion: "Gasolina", velocidad: 341, potencia: 720, aceleracion: 2.9, tipo: "Superdeportivo", precio: 300000, 
+    { id: 5, nombre: "McLaren 720S", año: 2017, chasis: "Coupé", propulsion: "Gasolina", velocidad: 341, potencia: 720, aceleracion: 2.9, tipo: "Superdeportivo", precio: 300000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/6/6c/McLaren_720S_Orange.jpg"  },
-    { nombre: "Audi RS e-tron GT", año: 2021, chasis: "Sedán", propulsion: "Eléctrico", velocidad: 250, potencia: 646, aceleracion: 3.3, tipo: "Eléctrico", precio: 140000, 
+    { id: 6, nombre: "Audi RS e-tron GT", año: 2021, chasis: "Sedán", propulsion: "Eléctrico", velocidad: 250, potencia: 646, aceleracion: 3.3, tipo: "Eléctrico", precio: 140000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Audi_RS_e-tron_GT_quattro_%28infobox%29.jpg/800px-Audi_RS_e-tron_GT_quattro_%28infobox%29.jpg?20210404120140"  },
-    { nombre: "Lamborghini Aventador", año: 2011, chasis: "Coupé", propulsion: "Gasolina", velocidad: 350, potencia: 740, aceleracion: 2.9, tipo: "Superdeportivo", precio: 400000, 
+    { id: 7, nombre: "Lamborghini Aventador", año: 2011, chasis: "Coupé", propulsion: "Gasolina", velocidad: 350, potencia: 740, aceleracion: 2.9, tipo: "Superdeportivo", precio: 400000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Lamborghini_Aventador_LP_750-4_SV.jpg/800px-Lamborghini_Aventador_LP_750-4_SV.jpg?20241004185835"  },
-    { nombre: "Koenigsegg Jesko", año: 2022, chasis: "Coupé", propulsion: "Gasolina", velocidad: 483, potencia: 1600, aceleracion: 2.5, tipo: "Hiperdeportivo", precio: 3000000, 
+    { id: 8, nombre: "Koenigsegg Jesko", año: 2022, chasis: "Coupé", propulsion: "Gasolina", velocidad: 483, potencia: 1600, aceleracion: 2.5, tipo: "Hiperdeportivo", precio: 3000000, 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Koenigsegg_Jesko.jpg/800px-Koenigsegg_Jesko.jpg?20211024022338"  }, 
-    { nombre: "Porsche 911 Carrera 4", año: 2012, chasis: "convertible de 2 puertas", propulsion: "gasolina", velocidad: 282, potencia: 345, aceleracion: 5.1, tipo: "Deportivo", precio: 280000, 
+    { id: 9, nombre: "Porsche 911 Carrera 4", año: 2012, chasis: "convertible de 2 puertas", propulsion: "gasolina", velocidad: 282, potencia: 345, aceleracion: 5.1, tipo: "Deportivo", precio: 280000, 
       imagen: "https://paddockmotors.es/wp-content/uploads/2024/03/DSC01257.webp" }
-    ,{ nombre: "Porsche 911 Carrera 4", año: "1998", chasis: "convertible de 2 puertas", propulsion: "gasolina", velocidad: "280", potencia: "296", aceleracion: "5,4", tipo: "Deportivo", precio: "280000", 
+    ,{ id: 10, nombre: "Porsche 911 Carrera 4", año: "1998", chasis: "convertible de 2 puertas", propulsion: "gasolina", velocidad: "280", potencia: "296", aceleracion: "5,4", tipo: "Deportivo", precio: "280000", 
       imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Porsche_911_%28996%29_Carrera_Cabriolet_%288516320329%29.jpg/800px-Porsche_911_%28996%29_Carrera_Cabriolet_%288516320329%29.jpg?20150109081035" }
 ];
