97f5f7cd86fe2f5c07c2011d7d47197bbbaf51e2
mlleonart <94650399+mlleonart@users.noreply.github.com>
2024-10-15 12:28:10 -0300

    Update load.js

--- load.js
+++ load.js
@@ -1,71 +1,61 @@
-// Función para renderizar la ficha del coche
-  function renderCarCard(car) {
-    return `
-       
-      	        <div class="card">
-          	        <div class="badge" style="background-color: ${car.badgeColor};">${car.tipo}</div>
-                   	<a title="Porsche 911 GT2 RS  Coupé" href="${car.imagen}">
-                    	<picture>
-  							<source srcset="${car.imagen}" type="image/webp">
-  							<source srcset="${car.imagen}" type="image/avif">
-  							<img src="${car.imagen}" alt="${car.nombre} ${car.año} características y ficha técnica" loading="lazy">
-						</picture>
-              		</a> 
-                          <div class="card-content">
-                              <div class="card-title"><span class="editable" contenteditable="true">${car.nombre} ${car.chasis}</span></div>
-                              <div class="card-subtitle"><span class="editable" contenteditable="true">${car.año}</span></div>
-                              <div class="card-info"><span class="editable" contenteditable="true">${car.chasis} | ${car.propulsion}</span></div>
-                              <div class="card-stats">
-                                  <div class="stat">
-                                      <div class="stat-title">Velocidad</div>
-                                      <div class="stat-value"><span class="editable" contenteditable="true">${car.velocidad}</span> km/h</div>
-                                  </div>
-                                  <div class="stat">
-                                      <div class="stat-title">Potencia</div>
-                                      <div class="stat-value"><span class="editable" contenteditable="true">${car.potencia}</span> HP</div>
-                                  </div>
-                                  <div class="stat">
-                                      <div class="stat-title">0-100 km/h</div>
-                                      <div class="stat-value"><span class="editable" contenteditable="true">${car.aceleracion}</span>s</div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-      
+        // Función para renderizar la ficha del coche
+        function renderCarCard(car) {
+            return `
+                <div class="card">
+                    <div class="badge" style="background-color: ${car.badgeColor};">${car.tipo}</div>
+                    <a title="${car.nombre} ${car.año}" href="${car.imagen}">
+                        <picture>
+                            <source srcset="${car.imagen}" type="image/webp">
+                            <source srcset="${car.imagen}" type="image/avif">
+                            <img src="${car.imagen}" alt="${car.nombre} ${car.año} características y ficha técnica" loading="lazy">
+                        </picture>
+                    </a> 
+                    <div class="card-content">
+                        <div class="card-title"><span class="editable" contenteditable="true">${car.nombre} ${car.chasis}</span></div>
+                        <div class="card-subtitle"><span class="editable" contenteditable="true">${car.año}</span></div>
+                        <div class="card-info"><span class="editable" contenteditable="true">${car.chasis} | ${car.propulsion}</span></div>
+                        <div class="card-stats">
+                            <div class="stat">
+                                <div class="stat-title">Velocidad</div>
+                                <div class="stat-value"><span class="editable" contenteditable="true">${car.velocidad}</span> km/h</div>
+                            </div>
+                            <div class="stat">
+                                <div class="stat-title">Potencia</div>
+                                <div class="stat-value"><span class="editable" contenteditable="true">${car.potencia}</span> HP</div>
+                            </div>
+                            <div class="stat">
+                                <div class="stat-title">0-100 km/h</div>
+                                <div class="stat-value"><span class="editable" contenteditable="true">${car.aceleracion}</span>s</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
 
-     `;
-  }
-  
-  // Función para cargar los datos desde GitHub y filtrar por ID
-  async function loadCarData(carId) {
-    try {
-      // const response = await fetch(vehiclesUrl);
-      //const cars = await response.json();
-      // const selectedCar = cars.find(car => car.id === carId);
-      
-      const selectedCar = initialDeck.find(car => car.id === carId);
-      
-      if (selectedCar) {
-            // Asignar la imagen de fondo al contenedor bg-photo
-            const bgPhotoContainer = document.querySelector('.bg-photo');
-            bgPhotoContainer.style.backgroundImage = `url(${selectedCar.imagen})`;
-            bgPhotoContainer.style.backgroundSize = 'cover';
-            bgPhotoContainer.style.backgroundPosition = 'center';
-            
-            const container = document.getElementById('container');
-            container.innerHTML = renderCarCard(selectedCar);
-        
-        // Actualizar el HTML con los datos del coche
-    	document.getElementById('car-title').textContent = `Detalles y Especificaciones para el Modelo: ${selectedCar.chasis} ${selectedCar.año}`;
-    	document.getElementById('car-specs').textContent = `Características Técnicas del Vehículo ${selectedCar.propulsion}: ${selectedCar.velocidad} Km/h, ${selectedCar.potencia} HP, ${selectedCar.aceleracion} s`;
-        
-      } else {
-        console.error('Coche no encontrado');
-      }
-    } catch (error) {
-      console.error('Error al cargar los datos:', error);
-    }
-  }
+        // Función para cargar los datos del coche
+        async function loadCarData(carId) {
+            try {
+                const selectedCar = initialDeck.find(car => car.id === carId);
+                
+                if (selectedCar) {
+                    // Asignar la imagen de fondo al contenedor bg-photo
+                    const bgPhotoContainer = document.querySelector('.bg-photo');
+                    bgPhotoContainer.style.backgroundImage = `url(${selectedCar.imagen})`;
+                    
+                    const container = document.getElementById('container');
+                    container.innerHTML = renderCarCard(selectedCar);
+                
+                    // Actualizar el HTML con los datos del coche
+                    document.getElementById('car-title').textContent = `Detalles y Especificaciones para el Modelo: ${selectedCar.chasis} ${selectedCar.año}`;
+                    document.getElementById('car-specs').textContent = `Características Técnicas del Vehículo ${selectedCar.propulsion}: ${selectedCar.velocidad} Km/h, ${selectedCar.potencia} HP, ${selectedCar.aceleracion} s`;
+                } else {
+                    console.error('Coche no encontrado');
+                }
+            } catch (error) {
+                console.error('Error al cargar los datos:', error);
+            }
+        }
 
-  // Cargar la ficha del coche con ID 2 (ejemplo: Porsche 911 Carrera 4)
-//  loadCarData(2);
+        // Cargar la ficha del coche con ID 1 (Porsche 911 Carrera 4)
+        loadCarData(1);
