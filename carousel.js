24e83aaa68d6e497f318f07bbc0485452ea44367
mlleonart <94650399+mlleonart@users.noreply.github.com>
2024-11-05 16:31:34 -0300

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -3,32 +3,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const prevBtn = document.getElementById("prevBtn");
     const nextBtn = document.getElementById("nextBtn");
     const imagesContainer = document.getElementById("carousel-images");
-    
-    let currentSlide = 0;
 
-    function updateCarousel() {
-        const totalSlides = slides.length;
-        const offset = -currentSlide * 100; // Calcular el desplazamiento
-        imagesContainer.style.transform = `translateX(${offset}%)`;
-        
-        // Mantener los botones habilitados según el índice actual
-        prevBtn.style.display = currentSlide === 0 ? "none" : "block";
-        nextBtn.style.display = currentSlide === totalSlides - 1 ? "none" : "block";
+    // Verifica si los elementos del carrusel están presentes
+    if (slides.length > 0 && prevBtn && nextBtn && imagesContainer) {
+        let currentSlide = 0;
+
+        function updateCarousel() {
+            const totalSlides = slides.length;
+            const offset = -currentSlide * 100; // Calcular el desplazamiento
+            imagesContainer.style.transform = `translateX(${offset}%)`;
+
+            // Mantener los botones habilitados según el índice actual
+            prevBtn.style.display = currentSlide === 0 ? "none" : "block";
+            nextBtn.style.display = currentSlide === totalSlides - 1 ? "none" : "block";
+        }
+
+        prevBtn.addEventListener("click", () => {
+            if (currentSlide > 0) {
+                currentSlide--;
+                updateCarousel();
+            }
+        });
+
+        nextBtn.addEventListener("click", () => {
+            if (currentSlide < slides.length - 1) {
+                currentSlide++;
+                updateCarousel();
+            }
+        });
+
+        updateCarousel(); // Llamar a la función al cargar para configurar la vista inicial
     }
-
-    prevBtn.addEventListener("click", () => {
-        if (currentSlide > 0) {
-            currentSlide--;
-            updateCarousel();
-        }
-    });
-
-    nextBtn.addEventListener("click", () => {
-        if (currentSlide < slides.length - 1) {
-            currentSlide++;
-            updateCarousel();
-        }
-    });
-
-    updateCarousel(); // Llamar a la función al cargar para configurar la vista inicial
 });
